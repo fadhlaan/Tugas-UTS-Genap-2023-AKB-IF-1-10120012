412eecb0527e284be8b0c840b545bb874f9541fb
Tugas UTS Genap 2023 AKB IF-1 10120012 <fadhlanputra19@gmail.com>
2023-05-30 19:04:23 +0700

    commit init

---
+++ app/src/main/java/com/fadhlan/uts_10120012/main/profile/ProfileActivity.java
@@ -0,0 +1,73 @@
+package com.fadhlan.uts_10120012.main.profile;
+//07 Juni 2022, 10119079, Rafi Khalifanan Azanan, IF2
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.fadhlan.uts_10120012.R;
+import com.fadhlan.uts_10120012.main.diary.CategoriActivity;
+
+
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        //Initialize And Assign Variable
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        //Set Home Selected
+        bottomNavigationView.setSelectedItemId(R.id.ProfileId);
+
+        //Perform ItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.ProfileId:
+                        startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.CatatanHarian:
+                        startActivity(new Intent(getApplicationContext(), CategoriActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.About:
+                        final Dialog dialog = new Dialog(ProfileActivity.this);
+
+                        //Memasang Title / Judul pada Custom Dialog
+                        dialog.setTitle("About");
+
+                        //Memasang Desain Layout untuk Custom Dialog
+                        dialog.setContentView(R.layout.custom_design_dialog);
+
+                        //Memasang Listener / Aksi saat tombol OK di Klik
+                        Button DialogButton = dialog.findViewById(R.id.ok);
+                        DialogButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                dialog.dismiss();
+                            }
+                        });
+
+                        dialog.show();
+                        return true;
+                }
+
+                return false;
+            }
+        });
+    }
+}
